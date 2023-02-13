ddd6cbafa5864421590cab5ee7f55e4cebdbc0e4
youbin <you8inpark@gmail.com>
2023-02-13 23:20:58 +0900

    today's ts

--- alias.ts
+++ alias.ts
@@ -4,20 +4,20 @@ let num: NumType = function (x, y) {
   return x + y;
 };
 
-// type 함수타입 = (a: string) => number;
+type 함수타입 = (a: string) => number;
 
-// let 함수: 함수타입 = function (a) {
-//   return 10;
-// };
+let 함수: 함수타입 = function (a) {
+  return 10;
+};
 
-// type InfoType = (a: number) => number;
+type InfoType = (a: number) => number;
 
-// let info = {
-//   name: "kim",
-//   plusOne: (a) {
-//     return a + 1;
-//   },
-//   changeName: () => {},
-// };
+let info = {
+  name: "kim",
+  plusOne: (a) {
+    return a + 1;
+  },
+  changeName: () => {},
+};
 
-// info.plusOne();
+info.plusOne();
